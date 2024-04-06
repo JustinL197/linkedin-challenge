8ceadb41f2818d287a6e45c1d00b07c65f01b1fd
JustinL197 <159653227+JustinL197@users.noreply.github.com>
2024-04-06 10:30:30 -0400

    dynamically updating profile elements

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+const username = "Justin Lee";
+const jobtitle = "Software Engineering Student";
+const profileImage = "./assets/images/man.png";
+
+const usernameElem = document.querySelectorAll(".data-username");
+const jobtitleElem = document.querySelector('.data-job-title');
+const profileImageElem = document.querySelectorAll('.data-profile-image');
+
+for (let el of usernameElem){
+    el.innerHTML = username;
+}
+
+jobtitleElem.innerHTML = jobtitle;
+
+for (let pro of profileImageElem){
+    pro.src = profileImage;
+}
